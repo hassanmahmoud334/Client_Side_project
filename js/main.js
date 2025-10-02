8d5de2fc038cb73f9a15072b4b149a809fc4fec8
hassanmahmoud334 <hassanmahmoud334@gmail.com>
2025-10-02 18:17:41 +0300

    edit css

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
-import { initNavbar } from './components/navbar.js';
 import { fetchProducts } from "./api/productAPI.js";
 import { renderProducts, renderCategories, searchProducts, setAllProducts } from "./pages/products.js";
 import { renderFavorites } from "./pages/favorites.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  initNavbar();
-});
 
 async function init() {
   const products = await fetchProducts();
